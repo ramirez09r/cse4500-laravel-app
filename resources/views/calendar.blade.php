3cc73b3b9c238833a9b1ad899056a9e6f6e6a1bd
ramirez09r <ramirez09r@gmail.com>
2022-03-08 18:14:52 -0800

    3/1 Updates

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -29,18 +29,7 @@
         $( document ).ready(function() {
             var calendar = new FullCalendar.Calendar($('#calendar')[0], {
                 initialView: 'dayGridMonth',
-                events:  [
-                    {
-                        "title": "CSE4500 Class",
-                        "start": "2022-02-23T17:30:00",
-                        "end": "2022-02-23T18:45:00"
-                    },
-                    {
-                        "title": "CSE4500 Class",
-                        "start": "2022-02-28T17:30:00",
-                        "end": "2022-02-28T18:45:00"
-                    },
-                ]
+                events:  '/events-feed'
             });
             calendar.render();
         })
--- routes/web.php
+++ routes/web.php
@@ -48,4 +48,27 @@ Route::get('/db-migrate', function(){
     echo Artisan::output();
 });
 */
+Route::get('/db-test', function () {
+    try {
+        echo \DB::connection()->getDatabaseName();
+    } catch (\Exception $e) {
+        echo 'None';
+    }
+});
+Route::get('/events-feed', function(){
+    $data=array(
+        array(
+            "title"=> "CSE4500 Class",
+            "start"=> "2022-02-23T17:30:00",
+            "end"=> "2022-02-23T18:45:00"
+        ),
+        array(
+            "title"=> "CSE4500 Class",
+            "start"=> "2022-02-28T17:30:00",
+            "end"=> "2022-02-28T18:45:00"
+        )
+    );
+    return json_encode($data);
 
+
+});
